adb71ba04216242e79c438e9ef89376307199aba
Nicolis Yuvienco <nickyuvienco@gmail.com>
2020-12-09 19:38:32 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+//Fade in on scroll
+const observerOptions = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.25,
+};
+
+function observerCallback(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // fade in observed elements that are in view
+      entry.target.classList.add('fadeIn');
+    }
+  });
+}
+
+const fadeElms = document.querySelectorAll('.fade');
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+fadeElms.forEach((el) => observer.observe(el));
+//end fade in on scroll
+
+//Smart scroll nav bar
+// add padding top to show content behind navbar
+$('body').css('padding-top', $('.navbar').outerHeight() + 'px');
+
+// detect scroll top or down
+if ($('.smart-scroll').length > 0) {
+  // check if element exists
+  var last_scroll_top = 0;
+  $(window).on('scroll', function () {
+    scroll_top = $(this).scrollTop();
+    if (scroll_top < last_scroll_top) {
+      $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
+    } else {
+      $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
+    }
+    last_scroll_top = scroll_top;
+  });
+}
+// end Smart scroll nav bar
